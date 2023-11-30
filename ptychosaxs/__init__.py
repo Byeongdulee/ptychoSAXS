321f0b6626c2b1d8c993a3df34ac7267f3c59bda
blee <35266961+byeongdu@users.noreply.github.com>
2023-11-29 19:25:37 -0600

    fly_test added

--- ptychosaxs/__init__.py
+++ ptychosaxs/__init__.py
@@ -1,5 +1,7 @@
 from .motions import hexapod, phi, acscontroller
 from .interferometers import qds
+import time
+import numpy as np
 
 def disconnect():
     hexapod.disconnect()
@@ -9,4 +11,29 @@ def disconnect():
 def connect(self):
     self.hexapod = motions.Hexapod(motions.hexapodIP)
     self.acscontroller.connect(motions.acsIP)
-    self.phi = motions.Axis(acscontroller, 0)    
+    self.phi = motions.Axis(acscontroller, 0)
+
+def fly_test(sec=2, dev=0):
+    
+    t0 = time.time()
+    t_point = []
+    t = 0
+    relpos = []
+    #abspos = []
+    t_point = []
+    k = 0
+    while (t - t0) < sec:
+        rel, abs = qds.get_position()
+        relpos.append([rel[0], rel[1], rel[2]])
+        #abspos.append([abs[0], abs[1], abs[2]])
+        time.sleep(0.0001)
+        t = time.time()
+        t_point.append(t)
+        k = k +1
+        if k==10:
+            hexapod.run_traj()
+    relpos = np.asarray(relpos)
+    #abspos = np.asarray(abspos)
+    t_point = np.asarray(t_point)
+    t_point = t_point-t0
+    return relpos, t_point
