3716191cb2d7d47de20fe8ec1a037ffc0bd92691
Byeongdu Lee <blee@aps.anl.gov>
2024-01-10 16:41:11 -0600

    dg645 updated.

--- tools/dg645.py
+++ tools/dg645.py
@@ -2,17 +2,29 @@ from instruments.srs import SRSDG645
 import quantities as pq
 from enum import IntEnum
 from instruments.util_fns import ProxyList
-address = "tcpip://164.54.122.66:5025"
+addressB = "tcpip://164.54.122.66:5025" # 12idb
+addressC = "tcpip://164.54.122.33:5025" # 12idc 
 #DG = ik.srs.SRSDG645.open_from_uri(address)
-UBZ_shutter_deadtime = 0.005   # 5 ms.
-software_init_deadtime = 0.001
-DG645_BurstmaxtimeLimit = 41
+UBZ_SHUTTER_DEADTIME = 0.005   # 5 ms.
+SOFTWARE_INIT_DEADTIME = 0.001
+DG645_BURST_MAX_TIME = 41
 # Example:
 # import dg645 as DG
-# a = DG.dg645_12ID.open_from_uri(DG.address)
+# a = DG.dg645_12ID.open_from_uri(DG.addressB)
 # a.pilatus(0.1, 5, 2)
 # a.instrument["shutter"].initime
 # a.instrument["shutter"].duration
+# a.instrument["shutter"].polarity
+# a.instrument["shutter"].level_amplitude
+# a.instrument["shutter"].level_offset
+
+TRSC_INTERNAL = 0
+TSRC_EXTERNAL_RISING_EGDES = 1
+TSRC_EXTERNAL_FALLING_EGDES = 2
+TSRC_SINGLE_SHOT_EXTERNAL_RISING_EGDES = 3
+TSRC_SINGLE_SHOT_EXTERNAL_FALLING_EGDES = 4
+TSRC_SINGLE_SHOT = 5
+TSRC_LINE = 6
 
 class _dg645Instrument(object):
     def __init__(self, ddg, chan):
@@ -56,6 +68,33 @@ class _dg645Instrument(object):
     def duration(self, newval):
         self._ddg.channel[2*(self.idx+1)+1].delay = (self._ddg.channel[2*(self.idx+1)], pq.Quantity(newval, "s"))
 
+    @property
+    def polarity(self):
+        endtime = self._ddg.output[self.idx].polarity
+        return endtime
+        
+    @polarity.setter
+    def polarity(self, newval):
+        self._ddg.output[self.idx].polarity = SRSDG645.LevelPolarity(newval)
+
+    @property
+    def level_amplitude(self):
+        endtime = self._ddg.output[self.idx].level_amplitude
+        return endtime
+        
+    @level_amplitude.setter
+    def level_amplitude(self, newval):
+        self._ddg.output[self.idx].level_amplitude = newval
+
+    @property
+    def level_offset(self):
+        endtime = self._ddg.output[self.idx].level_offset
+        return endtime
+        
+    @level_offset.setter
+    def level_offset(self, newval):
+        self._ddg.output[self.idx].level_offset = newval
+
     
 
 class dg645_12ID(SRSDG645):
@@ -78,10 +117,11 @@ class dg645_12ID(SRSDG645):
         """ 
         definition of instruments
         """
-        shutter = 0
-        detector = 1
-        struck = 2
-        inhibitor = 3
+        base = 0
+        shutter = 1
+        detector = 2
+        struck = 3
+        inhibitor = 4
     
     @property
     def instrument(self):
@@ -105,36 +145,131 @@ class dg645_12ID(SRSDG645):
         self.set_pilatus(DGexpt, *kwd)
         self.trigger()
         
-    def set_pilatus(self, DGexpt, *kwd):
-        DGNimage = 1
-        Cycperiod = DGexpt + 1 # no meaning.        
-        if len(kwd)==0:
-            self.trigger_source = 5
-        elif (len(kwd)==1):
-            self.trigger_source = kwd
-        elif (len(kwd)>2):
-            DGNimage = kwd[0]
-            Cycperiod = kwd[1]
-        if (len(kwd)==3):
-            self.trigger_source = kwd[2]
+    def set_pilatus(self, DGexpt, trigger_source=5,DGNimage=1,Cycperiod=0):
+        delaytime = 0
+        if (delaytime >= UBZ_SHUTTER_DEADTIME):
+            delaytime = delaytime - UBZ_SHUTTER_DEADTIME
+        self.instrument["struck"].polarity = 0
+        self.instrument["detector"].polarity = 1
+        
+        self.trigger_source = trigger_source
+
         if (DGNimage>1):
             if (DGexpt >= Cycperiod):
                 raise ValueError("Image period should be longer than the exposure time + 0.004")
-        self.instrument["shutter"].initime = 0    
-        self.instrument["shutter"].duration = DGexpt+UBZ_shutter_deadtime*2
-        self.instrument["detector"].initime = UBZ_shutter_deadtime
-        self.instrument["detector"].duration = 0.001
-        self.instrument["struck"].initime = UBZ_shutter_deadtime
-        self.instrument["struck"].duration = 0.001
-        self.instrument["inhibitor"].initime = 0
-        self.instrument["inhibitor"].duration = 0
-        Cycdelay = software_init_deadtime
-        if ((Cycperiod < DG645_BurstmaxtimeLimit) and (DGNimage > 1)):
+        detector_delay = delaytime + UBZ_SHUTTER_DEADTIME
+        self.instrument["shutter"].initime = delaytime
+        self.instrument["shutter"].duration = DGexpt + UBZ_SHUTTER_DEADTIME*2
+        self.instrument["detector"].initime = detector_delay
+        self.instrument["detector"].duration = DGexpt
+        self.instrument["struck"].initime = detector_delay + DGexpt + 0.00001
+        self.instrument["struck"].duration = 0.0001
+        self.instrument["inhibitor"].initime = detector_delay
+        self.instrument["inhibitor"].duration = DGexpt
+
+        longesttime = delaytime + DGexpt + UBZ_SHUTTER_DEADTIME*2
+        if (detector_delay+DGexpt) > longesttime:
+            longesttime = detector_delay+DGexpt
+        if (Cycperiod < longesttime):
+            raise TimeoutError("Cycperiod should be longer than a single pulse")
+        Cycdelay = SOFTWARE_INIT_DEADTIME
+        
+        self.burst_init()
+
+        if ((Cycperiod < DG645_BURST_MAX_TIME) and (DGNimage > 1)):
             self.burst_set(DGNimage, Cycperiod, Cycdelay)
             self.check_error()
         else:
             self.burst_enable = 0
-            
+    
+    def set_pilatus2(self, DGexpt, DGNimage=1, Cycperiod=0, negativedelay=0, triggerDelay=0, trigger_source=5):
+        delaytime = 0
+        if (delaytime >= UBZ_SHUTTER_DEADTIME):
+            delaytime = delaytime - UBZ_SHUTTER_DEADTIME
+        self.instrument["struck"].polarity = 0
+        self.instrument["detector"].polarity = 1
+        
+        self.trigger_source = trigger_source
+
+        dA = negativedelay+0 # shutter
+        lAB = DGexpt+UBZ_SHUTTER_DEADTIME*2
+        dC = negativedelay+UBZ_SHUTTER_DEADTIME  # Pilatus
+        lCD = 0.001
+        
+        dE = dC		# Struck
+        lEF = lCD
+        
+        dG = triggerDelay
+        lGH = DGexpt		# Struck Inhibitor -- this should be come from Detector.
+
+        Cycdelay = SOFTWARE_INIT_DEADTIME
+        self.burst_init()
+        if (DGNimage > 1):
+            if (DGexpt>=Cycperiod):
+                raise TimeoutError("Cycperiod should be longer than a single pulse")
+    
+        self.instrument["shutter"].initime = dA
+        self.instrument["shutter"].duration = lAB
+        self.instrument["detector"].initime = dC
+        self.instrument["detector"].duration = lCD
+        self.instrument["struck"].initime = dE
+        self.instrument["struck"].duration = lEF
+        self.instrument["inhibitor"].initime = dG
+        self.instrument["inhibitor"].duration = lGH
+        if ((Cycperiod < DG645_BURST_MAX_TIME) and (DGNimage > 1)):
+            self.burst_set(DGNimage, Cycperiod, Cycdelay)
+            self.check_error()
+        else:
+            self.burst_enable = 0
+
+    def set_pilatus_fly(self, DGexpt, DGNimage=1, Cycperiod=0,trigger_source=5):
+
+        self.instrument["struck"].polarity = 0
+        self.instrument["detector"].polarity = 1
+
+        self.trigger_source = trigger_source
+
+        delaytime = 0
+    
+        dA = delaytime # shutter
+        lAB = DGexpt
+
+        dC = delaytime  # Pilatus
+        lCD = DGexpt
+	
+        dE = dC+DGexpt+0.00001		# Struck channel advance.
+        lEF = 0.0001
+	
+        dG = dC
+        lGH = DGexpt
+        longesttime = dA+lAB
+        if ((dE+lEF) > longesttime) :
+            longesttime = dE+lEF
+        if (Cycperiod < longesttime) :
+            raise TimeoutError("Cycperiod should be longer than a single pulse")
+
+        Cycdelay = SOFTWARE_INIT_DEADTIME
+
+        self.burst_init()
+        if (DGNimage > 1):
+            if (DGexpt >= Cycperiod):
+                raise TimeoutError("Cycperiod should be longer than a single pulse")
+
+        self.instrument["shutter"].initime = dA
+        self.instrument["shutter"].duration = lAB
+        self.instrument["detector"].initime = dC
+        self.instrument["detector"].duration = lCD
+        self.instrument["struck"].initime = dE
+        self.instrument["struck"].duration = lEF
+        self.instrument["inhibitor"].initime = dG
+        self.instrument["inhibitor"].duration = lGH
+
+        if ((Cycperiod < DG645_BURST_MAX_TIME) and (DGNimage > 1)):
+            self.burst_set(DGNimage, Cycperiod, Cycdelay)
+            self.check_error()
+        else:
+            self.burst_enable = 0
+
     def PE(self, DGexpt, *kwd):
         self.set_PE(DGexpt, *kwd)
         self.trigger()
@@ -155,15 +290,15 @@ class dg645_12ID(SRSDG645):
             if (DGexpt >= Cycperiod):
                 raise ValueError("Image period should be longer than the exposure time + 0.004")
         self.instrument["shutter"].initime = 0    
-        self.instrument["shutter"].duration = DGexpt+UBZ_shutter_deadtime*2
-        self.instrument["detector"].initime = UBZ_shutter_deadtime
+        self.instrument["shutter"].duration = DGexpt+UBZ_SHUTTER_DEADTIME*2
+        self.instrument["detector"].initime = UBZ_SHUTTER_DEADTIME
         self.instrument["detector"].duration = DGexpt
         self.instrument["struck"].initime = 0
         self.instrument["struck"].duration = 0.001
-        self.instrument["inhibitor"].initime = UBZ_shutter_deadtime
+        self.instrument["inhibitor"].initime = UBZ_SHUTTER_DEADTIME
         self.instrument["inhibitor"].duration = DGexpt
-        Cycdelay = software_init_deadtime
-        if ((Cycperiod < DG645_BurstmaxtimeLimit) and (DGNimage > 1)):
+        Cycdelay = SOFTWARE_INIT_DEADTIME
+        if ((Cycperiod < DG645_BURST_MAX_TIME) and (DGNimage > 1)):
             self.burst_set(DGNimage, Cycperiod, Cycdelay)
             self.check_error()
         else:
@@ -177,6 +312,11 @@ class dg645_12ID(SRSDG645):
         if rtn>0:
             raise IOError("Error in setting DG645")
         
+    def burst_init(self):
+        self.enable_burst_mode = 1
+        self.burst_delay = 0
+        self.enable_burst_t0_first = 0
+    
     def burst_set(self, Ncycle, Period, delay):
         self.burst_cycle = Ncycle
         self.burst_period = Period
--- tools/files.py
+++ tools/files.py
@@ -19,17 +19,3 @@ def create_dir(dirname):
 		print("Creating directory:"+dirname)
 	else:
 		print("The directory already exists!")
-
-
-def keepshutteropen():
-	val = caget("PB:12ID:STA_C_SCS_CLSD_PL.VAL")
-	if val == 0:
-		caput("12ida2:rShtrC:Open", 1)
-
-class keepshopenThread(Thread):
-	def run(self):
-		while self.needOn:
-			keepshutteropen()
-	def stop(self):
-		self.needOn = False
-		
