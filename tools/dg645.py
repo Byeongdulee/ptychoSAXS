64674c3cc8c27c67d11514539e45f103e2c9dfe4
Byeongdu Lee <blee@aps.anl.gov>
2024-01-15 10:36:37 -0600

    Update dg645.py

--- tools/dg645.py
+++ tools/dg645.py
@@ -3,8 +3,12 @@ import quantities as pq
 from enum import IntEnum
 from instruments.util_fns import ProxyList
 import time
-addressB = "tcpip://164.54.122.66:5025" # 12idb
-addressC = "tcpip://164.54.122.33:5025" # 12idc 
+
+BEAMLINE_12IDB = 0
+BEAMLINE_12IDC = 1
+
+ADDRESS_12IDB = "tcpip://164.54.122.66:5025" # 12idb
+ADDRESS_12IDC = "tcpip://164.54.122.33:5025" # 12idc 
 #DG = ik.srs.SRSDG645.open_from_uri(address)
 UBZ_SHUTTER_DEADTIME = 0.005   # 5 ms.
 SOFTWARE_INIT_DEADTIME = 0.001
@@ -13,8 +17,8 @@ DG645_BURST_MAX_TIME = 41
 # import dg645 as DG
 # a = DG.dg645_12ID.open_from_uri(DG.addressB)
 # a.pilatus(0.1, 5, 2)
-# a.instrument["shutter"].initime
-# a.instrument["shutter"].duration
+# a.instrument["shutter"].delay
+# a.instrument["shutter"].pulsewidth
 # a.instrument["shutter"].polarity
 # a.instrument["shutter"].level_amplitude
 # a.instrument["shutter"].level_offset
@@ -26,6 +30,67 @@ TSRC_SINGLE_SHOT_EXTERNAL_RISING_EGDES = 3
 TSRC_SINGLE_SHOT_EXTERNAL_FALLING_EGDES = 4
 TSRC_SINGLE_SHOT = 5
 TSRC_LINE = 6
+
+INHIBIT_OFF = 0
+INHIBIT_TRIGGERS = 1
+INHIBIT_AB = 2
+INHIBIT_AB_AND_CD = 3
+INHIBIT_AB_CD_AND_EF = 4
+INHIBIT_AB_CD_EF_AND_GH = 5
+
+PRESCALE_TRIGGER_INPUT = 0
+PRESCALE_OUTPUT_AB = 1
+PRESCALE_OUTPUT_CD = 2
+PRESCALE_OUTPUT_EF = 3
+PRESCALE_OUTPUT_GH = 4
+
+'''
+Trigger holdoff : 
+Trigger holdoff sets the minimum allowed time between successive triggers.
+For example, if the trigger holdoff is set to 10 µs, then successive triggers 
+will be ignored until at least 10 µs have passed since the last trigger. 
+The red RATE LED will flash with each ignored trigger. Specifying holdoff is 
+useful if a trigger event in your application generates a significant noise 
+transient that must have time to decay away before the next trigger is generated.  
+Trigger holdoff can also be used to trigger the DG645 at a sub-multiple of 
+a known input trigger rate. For example, by selecting LINE as the trigger source 
+and setting the holdoff to 0.99 s, the DG645 can be triggered synchronously with 
+the power line, but at 1 Hz. This technique works as long as the timebase of 
+the trigger source doesn't vary significantly relative to the DG645's timebase. 
+Otherwise, trigger prescaling should be used. 
+Note that trigger holdoff is available only after advanced triggering is enabled. 
+Once advanced triggering is enabled, the user can view and modify the trigger 
+holdoff by successively pressing the 'TRIG' key in the DISPLAY section of 
+the front panel until the display prefix is 'HOLD'. 
+If the trigger holdoff is 10 µs, then the main display will show 'HOLD 0.000010000000', 
+and the STATUS LED just below the main display will be highlighted. 
+Once displayed, the user can modify the trigger holdoff using any of the methods 
+discussed in the section Front-Panel Interface earlier in this chapter. 
+
+
+Trigger Prescaling:
+The DG645 supports a number of complex triggering requirements through a set of 
+prescaling registers. Trigger prescaling enables the DG645 to be triggered synchronously 
+with a much faster source, but at a sub-multiple of the original trigger frequency. 
+For example, the DG645 can be triggered at 1 kHz, but synchronously with a mode locked laser 
+running at 80 MHz, by prescaling the trigger input by 80,000.  
+Furthermore, the DG645 also contains a separate prescaler for each front panel output 
+that enables it to operate at a sub-multiple of the prescaled trigger input frequency. 
+Continuing with the example above, if the AB prescaler is set to 100, the AB output will 
+only be enabled for 1 out of every 100 delay cycles which is equivalent to a rate of 
+1 kHz/100 = 10 Hz. 
+Lastly, the DG645 contains a separate phase register for each output prescaler 
+that determines the phase of the prescaler output relative to the other prescaled outputs. 
+For example, if both the AB and CD prescalers are set to 100 and their phase registers to 
+0 and 50, respectively, then AB and CD will both run at 10 Hz, but CD's output will be 
+enabled 50 delay cycles after AB's output. 
+'''
+def connect(beamline=BEAMLINE_12IDB):
+    if beamline==BEAMLINE_12IDB:
+        addr = ADDRESS_12IDB
+    if beamline==BEAMLINE_12IDC:
+        addr = ADDRESS_12IDC
+    return dg645_12ID.open_from_uri(addr)
 
 class _dg645Instrument(object):
     def __init__(self, ddg, chan):
@@ -48,26 +113,26 @@ class _dg645Instrument(object):
         return self._chan
 
     @property
-    def initime(self):
+    def delay(self):
         endtime = self._ddg.channel[2*self.idx].delay
 #        if not (endtime[0] == self._ddg.Channels.T0):
 #            self._ddg.channel[2*self.idx+1].delay = (self._ddg.channel[0], endtime[1])
         return endtime[1]
 
-    @initime.setter
-    def initime(self, newval):
+    @delay.setter
+    def delay(self, newval):
         #self._ddg.channel[2*self.idx+1].delay = (self._ddg.channel[0], pq.Quantity(newval, "s"))
         self._ddg.channel[2*self.idx].delay = (self._ddg.channel[0], newval)
 
     @property
-    def duration(self):
+    def pulsewidth(self):
         endtime = self._ddg.channel[2*self.idx+1].delay
 #        if not (endtime[0] == self._ddg.Channels((2*self.idx+1))):
 #            self._ddg.channel[2*self.idx+1].delay = (self._ddg.channel[2*self.idx], endtime[1])
         return endtime[1]
         
-    @duration.setter
-    def duration(self, newval):
+    @pulsewidth.setter
+    def pulsewidth(self, newval):
 #        self._ddg.channel[2*self.idx+1].delay = (self._ddg.channel[2*self.idx], pq.Quantity(newval, "s"))
         self._ddg.channel[2*self.idx+1].delay = (self._ddg.channel[2*self.idx], newval)
 
@@ -101,16 +166,17 @@ class _dg645Instrument(object):
     
 
 class dg645_12ID(SRSDG645):
-#######################################################
-#######################################################
-# For DG645 at 12ID-B
-# Front 2 BNCs will be used: 
-#	AB for shutter
-#	CD for Detector 
-#	EF for struck
-# Need to use "Burtst mode", which determine the number of delay cycle or shots.
-# "Single shot triggering" will trigger N bursts.
-#######################################################
+    '''
+        For DG645 at 12ID-B
+        Front 2 BNCs will be used: 
+            AB for shutter
+            CD for Detector 
+            EF for struck
+            GH for inhibitor
+            
+        Need to use "Burtst mode", which determine the number of delay cycle or shots.
+        "Single shot triggering" will trigger N bursts.
+    '''
     
     def __init__(self, filelike):
         super(SRSDG645, self).__init__(filelike)
@@ -136,9 +202,9 @@ class dg645_12ID(SRSDG645):
         >>> inst = ik.srs.SRSDG645.open_gpibusb('/dev/ttyUSB0', 1)
         >>> inst.instrument[inst.Instruments.shutter]
         >>> inst.instrument["shutter"]
-        >>> inst.instrument["shutter"].initime
-        >>> inst.instrument["shutter"].duration
-        >>> inst.instrument["shutter"].initime = 0.1
+        >>> inst.instrument["shutter"].delay
+        >>> inst.instrument["shutter"].pulsewidth
+        >>> inst.instrument["shutter"].delay = 0.1
         See the example in `dg645_12ID` for a more complete example.
         :rtype: `_dg645Instrument`
         """
@@ -161,14 +227,14 @@ class dg645_12ID(SRSDG645):
             if (DGexpt >= Cycperiod):
                 raise ValueError("Image period should be longer than the exposure time + 0.004")
         detector_delay = delaytime + UBZ_SHUTTER_DEADTIME
-        self.instrument["shutter"].initime = delaytime
-        self.instrument["shutter"].duration = DGexpt + UBZ_SHUTTER_DEADTIME*2
-        self.instrument["detector"].initime = detector_delay
-        self.instrument["detector"].duration = DGexpt
-        self.instrument["struck"].initime = detector_delay + DGexpt + 0.00001
-        self.instrument["struck"].duration = 0.0001
-        self.instrument["inhibitor"].initime = detector_delay
-        self.instrument["inhibitor"].duration = DGexpt
+        self.instrument["shutter"].delay = delaytime
+        self.instrument["shutter"].pulsewidth = DGexpt + UBZ_SHUTTER_DEADTIME*2
+        self.instrument["detector"].delay = detector_delay
+        self.instrument["detector"].pulsewidth = DGexpt
+        self.instrument["struck"].delay = detector_delay + DGexpt + 0.00001
+        self.instrument["struck"].pulsewidth = 0.0001
+        self.instrument["inhibitor"].delay = detector_delay
+        self.instrument["inhibitor"].pulsewidth = DGexpt
 
         longesttime = delaytime + DGexpt + UBZ_SHUTTER_DEADTIME*2
         if (detector_delay+DGexpt) > longesttime:
@@ -211,21 +277,21 @@ class dg645_12ID(SRSDG645):
             if (DGexpt>=Cycperiod):
                 raise TimeoutError("Cycperiod should be longer than a single pulse")
     
-        self.instrument["shutter"].initime = dA
-        self.instrument["shutter"].duration = lAB
-        self.instrument["detector"].initime = dC
-        self.instrument["detector"].duration = lCD
-        self.instrument["struck"].initime = dE
-        self.instrument["struck"].duration = lEF
-        self.instrument["inhibitor"].initime = dG
-        self.instrument["inhibitor"].duration = lGH
+        self.instrument["shutter"].delay = dA
+        self.instrument["shutter"].pulsewidth = lAB
+        self.instrument["detector"].delay = dC
+        self.instrument["detector"].pulsewidth = lCD
+        self.instrument["struck"].delay = dE
+        self.instrument["struck"].pulsewidth = lEF
+        self.instrument["inhibitor"].delay = dG
+        self.instrument["inhibitor"].pulsewidth = lGH
         if ((Cycperiod < DG645_BURST_MAX_TIME) and (DGNimage > 1)):
             self.burst_set(DGNimage, Cycperiod, Cycdelay)
             self.check_error()
         else:
             self.burst_enable = 0
 
-    def set_pilatus_fly(self, DGexpt, DGNimage=1, Cycperiod=0,trigger_source=5):
+    def set_pilatus_fly(self, DGexpt, DGNimage=1, Cycperiod=0,trigger_source=TSRC_EXTERNAL_RISING_EGDES):
 
         self.instrument["struck"].polarity = 0
         self.instrument["detector"].polarity = 1
@@ -259,22 +325,22 @@ class dg645_12ID(SRSDG645):
             if (DGexpt >= Cycperiod):
                 raise TimeoutError("Cycperiod should be longer than a single pulse")
 #        print(dA, lAB, dC, lCD, dE, lEF, dG, lGH)
-        self.instrument["shutter"].initime = dA
+        self.instrument["shutter"].delay = dA
         time.sleep(0.1)
 #        self.check_error()
-        self.instrument["shutter"].duration = lAB
+        self.instrument["shutter"].pulsewidth = lAB
 #        self.check_error()
-        self.instrument["detector"].initime = dC
+        self.instrument["detector"].delay = dC
 #        self.check_error()
-        self.instrument["detector"].duration = lCD
+        self.instrument["detector"].pulsewidth = lCD
 #        self.check_error()
-        self.instrument["struck"].initime = dE
+        self.instrument["struck"].delay = dE
 #        self.check_error()
-        self.instrument["struck"].duration = lEF
+        self.instrument["struck"].pulsewidth = lEF
 #        self.check_error()
-        self.instrument["inhibitor"].initime = dG
+        self.instrument["inhibitor"].delay = dG
 #        self.check_error()
-        self.instrument["inhibitor"].duration = lGH
+        self.instrument["inhibitor"].pulsewidth = lGH
 #        self.check_error()
 
         if ((Cycperiod < DG645_BURST_MAX_TIME) and (DGNimage > 1)):
@@ -303,14 +369,14 @@ class dg645_12ID(SRSDG645):
         if (DGNimage>1):
             if (DGexpt >= Cycperiod):
                 raise ValueError("Image period should be longer than the exposure time + 0.004")
-        self.instrument["shutter"].initime = 0    
-        self.instrument["shutter"].duration = DGexpt+UBZ_SHUTTER_DEADTIME*2
-        self.instrument["detector"].initime = UBZ_SHUTTER_DEADTIME
-        self.instrument["detector"].duration = DGexpt
-        self.instrument["struck"].initime = 0
-        self.instrument["struck"].duration = 0.001
-        self.instrument["inhibitor"].initime = UBZ_SHUTTER_DEADTIME
-        self.instrument["inhibitor"].duration = DGexpt
+        self.instrument["shutter"].delay = 0    
+        self.instrument["shutter"].pulsewidth = DGexpt+UBZ_SHUTTER_DEADTIME*2
+        self.instrument["detector"].delay = UBZ_SHUTTER_DEADTIME
+        self.instrument["detector"].pulsewidth = DGexpt
+        self.instrument["struck"].delay = 0
+        self.instrument["struck"].pulsewidth = 0.001
+        self.instrument["inhibitor"].delay = UBZ_SHUTTER_DEADTIME
+        self.instrument["inhibitor"].pulsewidth = DGexpt
         Cycdelay = SOFTWARE_INIT_DEADTIME
         if ((Cycperiod < DG645_BURST_MAX_TIME) and (DGNimage > 1)):
             self.burst_set(DGNimage, Cycperiod, Cycdelay)
@@ -394,3 +460,33 @@ class dg645_12ID(SRSDG645):
     @burst_enable.setter
     def burst_enable(self, newval):
         self.sendcmd("BURM {}".format(1 if newval else 0))
+
+    @property
+    def inhibit(self):
+        """
+        Gets/sets whether inhibit.
+        use after enable_adv_triggering
+        This requires inhibitor signal to the BNC in the backpanel.
+        :type: `int`
+        """
+        return int(self.query("INHB?"))
+
+    @inhibit.setter
+    def inhibit(self, newval=INHIBIT_TRIGGERS):
+        self.sendcmd("INHB {}".format(int(newval)))
+
+    '''Prescale is not available for my dg645.'''
+    # @property
+    # def prescale_factor(self):
+    #     """
+    #     Gets/sets prescale factor.
+    #     use after enable_adv_triggering
+    #     :type: `int`
+    #     """
+    #     resp = self.query("PRES?").split(",")
+    #     return (int(resp[0]), int(resp[1]))
+
+    # @prescale_factor.setter
+    # def prescale_factor(self, newval=PRESCALE_TRIGGER_INPUT, factor=10):
+    #     # default: taking only every 10 trigger inputs. 
+    #     self.sendcmd("PRES {},{}".format(int(newval), int(factor)))
