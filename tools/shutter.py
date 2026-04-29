c73bd14f1f55f4280d72d9757ab917986b322adf
Lee <blee@anl.gov>
2026-04-28 22:05:52 -0500

    Update shutter.py

--- tools/shutter.py
+++ tools/shutter.py
@@ -1,29 +1,51 @@
 from threading import Thread
 from epics import caget, caput, PV
 import time
-import QtCore
+from PyQt5 import QtCore
 from PyQt5.QtCore import QObject
 
-def keepshutteropen():
-	val = caget("PB:12ID:STA_C_SCS_CLSD_PL.VAL")
-	if val == 0:
-		caput("12ida2:rShtrC:Open", 1)
+beamline = '12IDC'
+
+def keepshutteropen(beamline='12IDC'):
+    if beamline.lower() == '12idb':
+        station = 'B'
+    if beamline.lower() == '12idc':
+        station = 'C'
+    pv_a_shutter = f"PB:12ID:STA_{station}_S{station}S_CLSD_PL.VAL"
+    pv_exp_shutter = f"12ida2:rShtr{station}:Open"
+    val = caget(pv_a_shutter)
+    if val == 0:
+        caput(pv_exp_shutter, 1)
 
 ## shutter stuff.. Could be eventually separated out.
-def open_shutter(self):
-    caput('12ida2:rShtrC:Open', 1)
+def open_shutter(beamline='12IDC'):
+    if beamline.lower() == '12idb':
+        station = 'B'
+    if beamline.lower() == '12idc':
+        station = 'C'
+    pv_exp_shutter = f"12ida2:rShtr{station}:Open"
+    caput(pv_exp_shutter, 1)
 
-def close_shutter(self):
-    caput('12ida2:rShtrC:Close', 1)
+def close_shutter(beamline='12IDC'):
+    if beamline.lower() == '12idb':
+        station = 'B'
+    if beamline.lower() == '12idc':
+        station = 'C'
+    pv_exp_shutter = f"12ida2:rShtr{station}:Close"
+    caput(pv_exp_shutter, 1)
 
 class beamstatus(QObject):
     onChange = QtCore.pyqtSignal()
-    def __init__(self):
+    def __init__(self, beamline="12IDC"):
+        if beamline.lower() == '12idb':
+            station = 'B'
+        if beamline.lower() == '12idc':
+            station = 'C'
         # A station shutter..
         self.shutter_val = PV('PB:12ID:STA_A_FES_CLSD_PL', callback=self.check_A_shutter)
         self.shutterA = PV('12ida2:rShtrA:Open')
-        self.shutterC_open = PV('12ida2:rShtrC:Open')
-        self.shutterC_close = PV('12ida2:rShtrC:Close')
+        self.shutterC_open = PV(f'12ida2:rShtr{station}:Open')
+        self.shutterC_close = PV(f'12ida2:rShtr{station}:Close')
 
     def check_A_shutter(self, value, **kws):
         if value == 0:
@@ -44,11 +66,15 @@ class beamstatus(QObject):
         self.shutterC_close.put(1)
 
 class shutter():
-    def __init__(self):
-        self.shutterC_open = PV('12ida2:rShtrC:Open')
-        self.shutterC_close = PV('12ida2:rShtrC:Close')
+    def __init__(self, beamline="12IDC"):
+        if beamline.lower() == '12idb':
+            station = 'B'
+        if beamline.lower() == '12idc':
+            station = 'C'
+        self.shutterC_open = PV(f'12ida2:rShtr{station}:Open')
+        self.shutterC_close = PV(f'12ida2:rShtr{station}:Close')
         self.shutterA_open = PV('12ida2:rShtrA:Open')
-        self.status = PV('PB:12ID:STA_C_SCS_CLSD_PL.VAL')
+        self.status = PV(f'PB:12ID:STA_{station}_S{station}S_CLSD_PL.VAL')
     def get_status(self):
         if self.status.get() == 1:
             # shutter is in close status
@@ -70,7 +96,7 @@ class shutter():
             if time.time()-t0>timout:
                 print("Shutter won't open in timeout (5s).")
                 break
-	
+    
     def close(self):
         self.shutterC_close.put(1)
         
@@ -79,8 +105,8 @@ class shutter():
 
 
 class keepshopenThread(Thread):
-	def run(self):
-		while self.needOn:
-			keepshutteropen()
-	def stop(self):
-		self.needOn = False
+    def run(self):
+        while self.needOn:
+            keepshutteropen()
+    def stop(self):
+        self.needOn = False
