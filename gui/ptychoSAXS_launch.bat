e2506ce8c452cc1380ae10eb5ff5d9f6ad714575
Byeongdu Lee <35266961+byeongdu@users.noreply.github.com>
2026-07-30 17:34:10 -0500

    Added buffer clearing to softglue

---
+++ gui/ptychoSAXS_launch.bat
@@ -0,0 +1,7 @@
+@echo off
+REM === Change this to your conda installation path ===
+CALL "C:\Users\s12idc\Anaconda3\Scripts\activate.bat"
+
+REM === Run your python script ===
+cd C:\Users\s12idc\Documents\GitHub\ptychoSAXS\gui\
+C:\Users\s12idc\Anaconda3\python.exe C:\Users\s12idc\Documents\GitHub\ptychoSAXS\gui\rungui.py
